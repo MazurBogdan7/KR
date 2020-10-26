74d244c5af426867356a84678853a135eae27711
MazurBogdan7 <alersanmazur@yandex.ru>
2020-10-26 12:51:14 +0300

    kontrol

--- kr1.c
+++ kr1.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+
 int main ()
 {
-	int fib, fib1, fib2, i, fac, step, inpu, resER, error;
+	int fib, fib1, i, fac, step, inpu, s, resER, error;
 
-	float sum, eps, num, mod ;
+    float sum, eps, num2, num1 ,mod ,cor;
 
-	error = 0 ;fib = 1; fib1 = 1; fib2 = 2; i = 2; sum = 0; num = 0; step = 6; fac = 1; 
+	error = 0 ;fib = 1; fib1 = 0; i = 2/*number in factorial*/; sum = 0.0; num1 = 0.0; num2 = 0.0; step = 6; fac = 1; cor = 0.0; s = 2;
 
 	inpu=scanf ("%f",&eps);
 	if(inpu < 0){
@@ -24,19 +25,21 @@ int main ()
 		do {
 			fib = fib+fib1;
 			fib1 = fib-fib1;
-			printf("%d\n",fib);
+			printf("%d\n",fib);//fib chek
 			fac = fac*i;
 			step = step*6;
-			
-			num = step*fac;
-			mod = fib/num;
+			cor = sqrt(s);
+			num1 = fib*cor;
+			num2 = step*fac;
+			mod = num1/num2;
 			i++;
+			s += 1;
 			sum = sum+mod;
 			} 
 		while (fabsf(mod) > eps);
 
-	resER=printf("%lf\n", (double)sum);
-	if (resER < 0){ 
+	resER=printf("%f\n", sum);
+	if (resER < 0.0){ 
 	
 		perror("Error\n");
 		error = 1;
